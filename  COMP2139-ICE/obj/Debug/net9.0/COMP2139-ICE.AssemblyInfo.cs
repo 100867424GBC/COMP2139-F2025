cd0e8f69b1a0970bea91f6d996095a531b44db04
100867424GBC <game.pass.pink@gmail.com>
2025-09-17 15:29:15 -0400

    Lab 2 Completed

---  COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
+++  COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2cc073f03c8f36d629102cbff47f8066d7ec74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6301dd6e2a0f59428947877fa7fce7099e5e69f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
